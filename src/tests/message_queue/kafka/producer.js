fb46c8cd75ca915ba68f4bf4fef8873af4bf08da
Hai Duc Vu <duchaivu1997@gmail.com>
2024-04-18 22:27:38 +0700

    Add kafka service

---
+++ src/tests/message_queue/kafka/producer.js
@@ -0,0 +1,23 @@
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['localhost:9092'],
+})
+
+const producer = kafka.producer()
+
+const runProducer = async () => {
+    await producer.connect()
+    await producer.send({
+        topic: 'test-topic',
+        messages: [
+            { value: 'Hello KafkaJS user12345!' },
+        ],
+    })
+
+    await producer.disconnect()
+}
+runProducer().catch((error) => {
+    console.log(error);
+});
